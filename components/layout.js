4d4add3d8db933a416718738800886125fddb8f9
Joe Rupertus <joerup2004@gmail.com>
2021-02-19 00:40:07 -0500

    Add files via upload

--- components/layout.js
+++ components/layout.js
@@ -3,8 +3,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Joe'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Joe Rupertus'
+export const siteTitle = 'Joe Rupertus'
 
 export default function Layout({ children, home }) {
   return (
@@ -28,7 +28,7 @@ export default function Layout({ children, home }) {
         {home ? (
           <>
             <img
-              src="/images/profile.jpg"
+              src="https://github.com/joerup2004/website/blob/main/Images/turtle%20icon.png?raw=true"
               className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
               alt={name}
             />
@@ -39,7 +39,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <a>
                 <img
-                  src="/images/profile.jpg"
+                  src="https://github.com/joerup2004/website/blob/main/Images/turtle%20icon.png?raw=true"
                   className={`${styles.headerImage} ${utilStyles.borderCircle}`}
                   alt={name}
                 />
--- pages/index.js
+++ pages/index.js
@@ -21,14 +21,11 @@ export default function Home ({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, my name is Joe</p>
-        <p>
-          Hello chickens
-        </p>
+        <p>Hello my friends. Welcome to the website.</p>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}>Stuff</h2>
 
         <ul className={utilStyles.list}>
           {allPostsData.map(post => (
