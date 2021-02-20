411d629cc60b2ab6497a48939f0bebf160ad9611
Joe Rupertus <joerup2004@gmail.com>
2021-02-20 11:58:19 -0500

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -29,36 +29,35 @@ export default function Home ({ allPostsData }) {
         <p>Hello my friends. Welcome to the website.</p>
       </section>
 
-      <section>
-        <button onClick={() => setCategory(viewCategory === 'apps' ? 'all' : 'apps')}>Apps</button>
-        <button onClick={() => setCategory(viewCategory === 'science' ? 'all' : 'science')}>Science</button>
-      </section>
+      {/* <section>
+        <button className={viewCategory == "all" ? styles.selectedbutton : styles.button} onClick={() => setCategory('all')}>All</button>
+        <button className={viewCategory == "apps" ? styles.selectedbutton : styles.button} onClick={() => setCategory(viewCategory === 'apps' ? 'all' : 'apps')}>Apps</button>
+        <button className={viewCategory == "space" ? styles.selectedbutton : styles.button} onClick={() => setCategory(viewCategory === 'space' ? 'all' : 'space')}>Space</button>
+      </section> */}
 
-      <section className={`${styles.headingMd} ${styles.padding1px}`}>
+      <section className={`${styles.headingMd}`}>
         <h2 className={styles.headingLg}>Posts</h2>
-        <ul className={styles.list}>
-          {allPostsData.map(({ id, category, subject, title, date, image, desc }) => (
-            <li className={styles.listItem} key={id} style={{ 
-              display: (viewCategory === category || viewCategory === 'all') ? "block":"none"
-            }}>
-            <Link href="/[category]/[id]" as={`/${category}/${id}`}>
-              <a>
-                <div className={styles.horizontal}>
-                  <div className={styles.image}>
-                    <img src={image}/>
-                  </div>
-                  <div>
-                    <h1>{`${category.toUpperCase()[0] + category.substr(1)} • ${subject}`}</h1>
-                    <h2>{title}</h2>
-                    <h3>{desc}</h3>
-                    <h4><Date dateString={date}/></h4>
-                  </div>
+        {allPostsData.map(({ id, category, subject, title, date, image, desc }) => (
+          <li className={styles.listItem} key={id} style={{ 
+            display: (viewCategory === category || viewCategory === 'all') ? "block":"none"
+          }}>
+          <Link href="/[category]/[id]" as={`/${category}/${id}`}>
+            <a>
+              <div className={styles.horizontal}>
+                <div className={styles.image}>
+                  <img src={image}/>
                 </div>
-              </a>
-            </Link>
-          </li>
-          ))}
-        </ul>
+                <div>
+                  <h1>{`${category.toUpperCase()[0] + category.substr(1)} • ${subject}`}</h1>
+                  <h2>{title}</h2>
+                  <h3>{desc}</h3>
+                  <h4><Date dateString={date}/></h4>
+                </div>
+              </div>
+            </a>
+          </Link>
+        </li>
+        ))}
       </section>
     </Layout>
   )
